f905e4b64fc76f06d1db0cd07649137eea276593
TuX <87657613+tuxby@users.noreply.github.com>
2022-10-05 10:32:46 +0300

    del 1 in Hello World

--- src/main/java/by/tux/Main.java
+++ src/main/java/by/tux/Main.java
@@ -1,5 +1,7 @@
-package by.tux;public class Main {
+package by.tux;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! 1");
+        System.out.println("Hello world!");
     }
 }
